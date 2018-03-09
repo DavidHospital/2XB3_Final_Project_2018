55ed46c8f4d8d77a255dc4a4044e46bbcaba4fc2
DavidHospital <david.hospital27@gmail.com>
2018-03-09 09:58:05 -0500

    Update DisasterEvent.java

--- app/src/main/java/group15/a2xb3/stormware/DisasterEvent.java
+++ app/src/main/java/group15/a2xb3/stormware/DisasterEvent.java
@@ -8,20 +8,48 @@ import java.util.Date;
  * Created by David on 2018-03-05.
  */
 
-public class DisasterEvent {
+public class DisasterEvent implement Comparable {
 
     private final String type;
     private final LatLng location;
-    private final Date date;
+    private final int year;
+	private final int month;
+	private final int day;
     private final float magnitude;
 
-    public DisasterEvent(String type, LatLng location, Date date, float magnitude) {
+    public DisasterEvent(String type, LatLng location, int year, int month, int day, float magnitude) {
         this.type = type;
         this.location = location;
-        this.date = date;
+        this.year = year;
+		this.month = month;
+		this.day = day;
         this.magnitude = magnitude;
     }
 
+	@Override
+	public int compareTo(Object o) {
+		DisasterEvent other = (DisasterEvent) o;
+		if (this.year < other.year) {
+			return -1;
+		} else if (this.year > other.year) {
+			return 1;
+		} else {
+			if (this.month < other.month) {
+				return -1;
+			} else if (this.month > other.month) {
+				return 1;
+			} else {
+				if (this.day < other.day) {
+					return -1;
+				} else if (this.day > other.day) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+		}
+	}
+	
     public String getType() {
         return type;
     }
@@ -30,10 +58,18 @@ public class DisasterEvent {
         return location;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
+	public float getYear() {
+		return year;
+	}
+	
+	public float getMonth() {
+		return month;
+	}
+	
+	public float getDay() {
+		return day;
+	}
+	
     public float getMagnitude() {
         return magnitude;
     }
