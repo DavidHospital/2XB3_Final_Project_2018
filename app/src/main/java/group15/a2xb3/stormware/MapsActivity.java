1b11beb05e0c182d1bfe5c373cc23b284d411c77
Elvis <chenz81@mcmaster.ca>
2018-03-09 00:01:44 -0500

    Search bar added

--- app/src/main/java/group15/a2xb3/stormware/MapsActivity.java
+++ app/src/main/java/group15/a2xb3/stormware/MapsActivity.java
@@ -25,6 +25,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    /**
+     * Implement search method for the search bar.
+     * @param view
+     */
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
+
 
     /**
      * Manipulates the map once available.
